a6014fd693f815db507fb3cc72e89847728f0df8
Raphael <raphael1grenier@gmail.com>
2023-10-07 21:29:20 -0400

    Ajouter une demande de confirmation de commande

--- gestionnaire_cartes.py
+++ gestionnaire_cartes.py
@@ -2,14 +2,16 @@ import os
 from carte_bingo import CreateurCarte
 import pickle
 from typing import List, Tuple
+from prettytable import PrettyTable
 
 
 class GestionnaireCarte:
     BD_TYPE = List[Tuple[str, CreateurCarte.Carte]]
+    OUI = {"oui", "yes", "o", "y"}
 
     def __init__(self) -> None:
         self.commande = []
-        
+
         # Vérifier si le fichier de base de données de carte existe:
         if not os.path.isfile("cartes_crees/base_données_cartes.json"):
             self.base_données: GestionnaireCarte.BD_TYPE = []
@@ -17,7 +19,7 @@ class GestionnaireCarte:
             liste_cartes = []
         else:
             # Ouvrir la base de données des cartes sauvegardées
-            with open("cartes_crees/base_données_cartes.json", "rb") as f:
+            with open("cartes_crees/base_données_cartes.pkl", "rb") as f:
                 self.base_données: GestionnaireCarte.BD_TYPE = pickle.load(f)
             liste_clients, liste_cartes = zip(*self.base_données)
             print(f"Il y a actuellement {len(set(liste_clients))} clients.")
@@ -28,10 +30,18 @@ class GestionnaireCarte:
         while self.demande_cartes():
             print("Les cartes seront générées seulement à la fin")
 
-        with open("cartes_crees/base_données_cartes.json", "wb") as f:
-            pickle.dump(self.base_données, f)
+        # Enregistrement de la commande dans la base de donnée
+        pt = PrettyTable(field_names=("Nom client", "Nombre de carte"))
+        pt.add_rows(self.commande)
+        if input("La commande est-elle valide?\n{}\n".format(pt)).lower() in GestionnaireCarte.OUI:
+            with open("cartes_crees/base_données_cartes.pkl", "wb") as f:
+                pickle.dump(self.base_données, f)
+            print("Commande sauvegardée")
+        else:
+            print("Commande annulée")
 
     def demande_cartes(self):
+        """Gestion d'une commande individuelle de carte"""
         try:
             client = input("Entrer le nom du client:\n")
             nombre_cartes = int(
@@ -44,7 +54,7 @@ class GestionnaireCarte:
         for i in range(nombre_cartes):
             self.base_données.append(
                 (client, CreateurCarte.nouvelle_carte(self.set_cartes)))
-        return input("Entrer d'autre commandes?\n").lower() in {"oui", "yes", "o", "y"}
+        return input("Entrer d'autre commandes?\n").lower() in GestionnaireCarte.OUI
 
 
 GestionnaireCarte()
