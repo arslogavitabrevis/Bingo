11309b9d87e950a832e0cf0db7e7b7e02e4a224e
Raphael <raphael1grenier@gmail.com>
2023-10-07 21:42:10 -0400

    Create a saving with the date

--- gestionnaire_cartes.py
+++ gestionnaire_cartes.py
@@ -3,23 +3,25 @@ from carte_bingo import CreateurCarte
 import pickle
 from typing import List, Tuple
 from prettytable import PrettyTable
+from datetime import datetime
 
 
 class GestionnaireCarte:
     BD_TYPE = List[Tuple[str, CreateurCarte.Carte]]
     OUI = {"oui", "yes", "o", "y"}
+    CHEMIN_SAUVEGARDE = "cartes_crees/base_données_cartes.pkl" 
 
     def __init__(self) -> None:
         self.commande = []
 
         # Vérifier si le fichier de base de données de carte existe:
-        if not os.path.isfile("cartes_crees/base_données_cartes.json"):
+        if not os.path.isfile(GestionnaireCarte.CHEMIN_SAUVEGARDE):
             self.base_données: GestionnaireCarte.BD_TYPE = []
             liste_clients = []
             liste_cartes = []
         else:
             # Ouvrir la base de données des cartes sauvegardées
-            with open("cartes_crees/base_données_cartes.pkl", "rb") as f:
+            with open(GestionnaireCarte.CHEMIN_SAUVEGARDE, "rb") as f:
                 self.base_données: GestionnaireCarte.BD_TYPE = pickle.load(f)
             liste_clients, liste_cartes = zip(*self.base_données)
             print(f"Il y a actuellement {len(set(liste_clients))} clients.")
@@ -34,7 +36,12 @@ class GestionnaireCarte:
         pt = PrettyTable(field_names=("Nom client", "Nombre de carte"))
         pt.add_rows(self.commande)
         if input("La commande est-elle valide?\n{}\n".format(pt)).lower() in GestionnaireCarte.OUI:
-            with open("cartes_crees/base_données_cartes.pkl", "wb") as f:
+            # Faire un enregistrement de sauvegarde
+            chemin_splité = GestionnaireCarte.CHEMIN_SAUVEGARDE.split(".")
+            date = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+            with open(f"{chemin_splité[0]}_{date}.{chemin_splité[1]}", "wb") as f:
+                pickle.dump(self.base_données, f)
+            with open(GestionnaireCarte.CHEMIN_SAUVEGARDE, "wb") as f:
                 pickle.dump(self.base_données, f)
             print("Commande sauvegardée")
         else:
